117424ed64c8b563a22f65b25398bde6b6f57c12
Seohyun-Roh <radult951@gmail.com>
2022-06-13 22:49:54 +0900

    feat: grammarly 페이지 반응형

--- front/src/routes/KGrammarly/index.tsx
+++ front/src/routes/KGrammarly/index.tsx
@@ -13,6 +13,10 @@ const KGrammarly = () => {
             <mark className={styles.wrong}>틀린 내용</mark> -&gt; <mark className={styles.right}>맞는 내용</mark>
             <p>틀린 내용에 대한 설명입니다.</p>
           </li>
+          <li className={styles.suggestionCard}>
+            <mark className={styles.wrong}>틀린 내용</mark> -&gt; <mark className={styles.right}>맞는 내용</mark>
+            <p>틀린 내용에 대한 설명입니다.</p>
+          </li>
         </ul>
       </div>
     </section>
