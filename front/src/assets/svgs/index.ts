574582350b23c7261a7c2d1ac77a854f576c6b8c
Seohyun-Roh <radult951@gmail.com>
2022-06-17 03:54:53 +0900

    feat: Grammarly 페이지 수정

--- front/src/assets/svgs/index.ts
+++ front/src/assets/svgs/index.ts
@@ -1,1 +1,4 @@
 export { ReactComponent as LogoImage } from './logo.svg';
+export { ReactComponent as Comment } from './icon-writing-box-paper.svg';
+export { ReactComponent as Document } from './icon-ai-powered.svg';
+export { ReactComponent as ArrowRight } from './arrow-right.svg';
