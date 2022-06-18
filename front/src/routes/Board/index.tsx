1c735ea478f9c1c4207e76dadb638c0428e97265
Seohyun-Roh <radult951@gmail.com>
2022-06-18 14:23:39 +0900

    google login test

--- front/src/routes/Board/index.tsx
+++ front/src/routes/Board/index.tsx
@@ -1,5 +1,18 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useMount } from 'react-use';
+
+const backendPortNumber = '5001';
+const serverUrl = `http://${window.location.hostname}:${backendPortNumber}/`;
+
 const Board = () => {
-  return <div>Board</div>;
+  const [posts, setPosts] = useState([]);
+
+  useMount(() => {
+    axios.get(`${serverUrl}board`).then((res) => setPosts(res.data.payload.posts));
+  });
+
+  return <div />;
 };
 
 export default Board;
--- front/src/routes/SignIn/index.tsx
+++ front/src/routes/SignIn/index.tsx
@@ -1,5 +1,25 @@
+import axios from 'axios';
+import { FormEvent } from 'react';
+
+const backendPortNumber = '5001';
+const serverUrl = `http://${window.location.hostname}:${backendPortNumber}/`;
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    axios.get(`${serverUrl}google`, { withCredentials: false }).then((res) => console.log(res));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <button id='google_login_btn' type='submit'>
+          ddd
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
