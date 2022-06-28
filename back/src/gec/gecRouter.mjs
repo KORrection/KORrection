1a2bcb1c89dded28c08e2888ebc5538297a2b922
GeonTae Park <rssr96@naver.com>
2022-06-28 14:17:54 +0900

    [fix] 주석처리

--- back/src/gec/gecRouter.mjs
+++ back/src/gec/gecRouter.mjs
@@ -10,12 +10,14 @@ gecRouter.post('/gec', async (req, res) => {
   const flaskUrl = 'http://kdt-ai4-team16.elicecoding.com:5000/predict';
   const bodyData = JSON.stringify(req.body);
   const testData = req.body.sentence.split('.');
+  console.log(testData);
   const result = [];
   for (let i = 0; i < testData.length; i++) {
     const text = testData[i];
     if (!text) {
       continue;
     }
+    console.log(text);
     const sentence = await axios
       .post(
         flaskUrl,
@@ -31,9 +33,17 @@ gecRouter.post('/gec', async (req, res) => {
 
     result.push(sentence.sentence);
   }
-
+  // const sentence = await axios
+  //   .post(flaskUrl, bodyData, {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     withCredentials: true,
+  //   })
+  //   .then((res) => res.data);
+  // console.log(result);
   res.json({ result });
 });
 export { gecRouter };
-//띄어쓰기로 ?
+
 //http://kdt-ai4-team16.elicecoding.com:5000/predict post 요청 주소
