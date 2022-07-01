9f4985265f0d58f2cd2da5a0f8979e3e3e5aaab4
Seohyun Roh <radult951@gmail.com>
2022-07-01 16:45:45 +0900

    feat: text change button

--- front/src/routes/GEC/Suggestion.tsx
+++ front/src/routes/GEC/Suggestion.tsx
@@ -1,4 +1,6 @@
 import { ArrowRight } from 'assets/svgs';
+import { useRecoilState } from 'recoil';
+import { gecTextState } from 'states/gec';
 
 import styles from './gec.module.scss';
 
@@ -8,6 +10,14 @@ interface IProps {
 }
 
 const Suggestion = ({ result, originalSentence }: IProps) => {
+  const [gecState, setGecState] = useRecoilState(gecTextState);
+
+  const handleSuggestionClick = (original: string, suggestion: string) => {
+    const newGecState = gecState.replace(original, suggestion);
+
+    setGecState(newGecState);
+  };
+
   return (
     <li className={styles.suggestionWrapper}>
       <ul>
@@ -16,8 +26,14 @@ const Suggestion = ({ result, originalSentence }: IProps) => {
 
           return (
             <li key={key} className={styles.suggestionCard}>
-              <mark className={styles.wrong}>{originalSentence}</mark> <ArrowRight />{' '}
-              <mark className={styles.right}>{res}</mark>
+              <button
+                type='button'
+                className={styles.suggestionButton}
+                onClick={() => handleSuggestionClick(originalSentence, res)}
+              >
+                <mark className={styles.wrong}>{originalSentence}</mark> <ArrowRight />{' '}
+                <mark className={styles.right}>{res}</mark>
+              </button>
             </li>
           );
         })}
