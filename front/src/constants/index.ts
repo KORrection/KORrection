34ff705ec3a4debf55e20c7c10d8b7538226a4e2
ParkGeonTae <rssr96@naver.com>
2022-07-02 10:13:16 +0000

    [fix] 분기나누기전 url수정

--- front/src/constants/index.ts
+++ front/src/constants/index.ts
@@ -1,22 +1,12 @@
 const BACKEND_PORT_NUMBER = '5001';
 
-<<<<<<< HEAD
-export const SERVER_URL = (() => {
-  if (process.env.NODE_ENV === 'development') {
-    return `${window.location.protocol}//${window.location.hostname}:${BACKEND_PORT_NUMBER}`;
-  }
-  return `https://${window.location.hostname}/api`;;
-})();
-=======
 // export const SERVER_URL = (() => {
-//   if (process.env.NODE_ENV === 'development') {
-//     return `${window.location.protocol}//${window.location.hostname}:${BACKEND_PORT_NUMBER}`;
-//   }
-//   return `http://${window.location.hostname}/api`;
+//  if (process.env.NODE_ENV === 'development') {
+//    return `${window.location.protocol}//${window.location.hostname}:${BACKEND_PORT_NUMBER}`;
+//  }
+//  return `https://${window.location.hostname}/api`;
 // })();
-
 export const SERVER_URL = `https://${window.location.hostname}/api`;
->>>>>>> ae2fada959b55321d3658fb2df6dab69d69983ff
 
 export const IMAGE_ON_ERROR_URL = 'https://www.yokogawa.com/public/img/default_image.png';
 
