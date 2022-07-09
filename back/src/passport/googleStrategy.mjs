de509276b3636e0e1194a4510a781aecb563f297
GeonTae Park <rssr96@naver.com>
2022-07-09 14:20:25 +0900

    사용자 등록시 이미지 랜덤

--- back/src/passport/googleStrategy.mjs
+++ back/src/passport/googleStrategy.mjs
@@ -2,7 +2,7 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { User } from '../user/userModel.mjs';
 import dotenv from 'dotenv';
-
+import aws from 'aws-sdk';
 dotenv.config();
 
 export default () => {
@@ -23,9 +23,11 @@ export default () => {
         } else {
           const newUser = await User.create({
             email,
-            nickname: 'null',
-            profilePicture: 'null',
-            description: 'null',
+            nickname: 'Anonymous',
+            profilePicture: `https://team16-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/${
+              Math.floor(Math.random() * 4) + 1
+            }.png`,
+            description: '자신을 소개해줄 메세지를 작성해주세요!',
             // socialId: profile.id, 나중에 소셜 로그인을 더 추가하면 비교값이 되는 변수
           });
           return done(null, newUser);
