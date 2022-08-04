d3609d809a1fe7557983e550291bf77f2038a3c9
Seohyun Roh <radult951@gmail.com>
2022-08-04 23:29:10 +0900

    fix quiz image url

--- front/src/constants/index.ts
+++ front/src/constants/index.ts
@@ -12,6 +12,6 @@ export const IMAGE_ON_ERROR_URL = 'https://www.yokogawa.com/public/img/default_i
 
 export const GEC_IMAGE_URL = 'https://blog.kakaocdn.net/dn/9QJ09/btrGhyPKh5R/c55Ucjn6VOWQkyo1hg4H51/img.gif';
 
-export const QUIZ_IMAGE_URL = 'https://blog.kakaocdn.net/dn/HMwHe/btrIISMbOiE/rTcIbn7bCL0i0Lcd7Ds8K1/img.gif';
+export const QUIZ_IMAGE_URL = 'https://blog.kakaocdn.net/dn/bInTew/btrIXPuneYN/5AY1FiWAT4ScqtGFLTpOH1/img.gif';
 
 export const BOARD_IMAGE_URL = 'https://blog.kakaocdn.net/dn/zuA0B/btrGgJ5jWdz/ImG4Dlup4kyeQxNPJIXuF1/img.gif';
